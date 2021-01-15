23800a4f02884c88bb59d14176a356804de3504c
Baesposito <48789539+Baesposito@users.noreply.github.com>
2021-01-15 15:00:01 +0100

    Add files via upload

--- Projet.py
+++ Projet.py
@@ -1,13 +1,8 @@
 class Projet():
     
-    
-    def __init__(self):
-        self.Donnee_Graphe = {}
-    
-    def get_coll(self):
-        return self.Donnee_Graphe
-    
-    def combinaison_mot(self,tab):
+    # initialisation des donnees pour la creation du graphe
+    def __init__(self,tab):
+        Donnee_Graphe = {}
         inc = 0
         for i in range(len(tab)):
             mot1 = tab[i][inc]
@@ -16,9 +11,17 @@ class Projet():
                 mot2 = tab[i][j]
                 k1 = mot1+mot2
                 k2 = mot2+mot1
-                if(not( k1 in self.Donnee_Graphe.keys() or k2 in self.Donnee_Graphe.keys() )):
-                    self.Donnee_Graphe[k1] = [mot1,mot2,1]
-                elif(k1 in self.Donnee_Graphe.keys()):
-                    self.Donnee_Graphe[k1][2] += 1
+                if(not( k1 in Donnee_Graphe.keys() or k2 in Donnee_Graphe.keys() )):
+                    Donnee_Graphe[k1] = [mot1,mot2,1]
+                elif(k1 in Donnee_Graphe.keys()):
+                    Donnee_Graphe[k1][2] += 1
                 else:
-                    self.Donnee_Graphe[k2][2] += 1
+                    Donnee_Graphe[k2][2] += 1
+    	
+        self.Donnee_Graphe = Donnee_Graphe # dictionnaire avec la forme expliquee dans le rapport
+    
+    # Recuperation du dictionnaire pour l'affichage
+    def get_coll(self):
+        return self.Donnee_Graphe
+    
+        
